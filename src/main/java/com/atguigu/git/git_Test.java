754f46b4e4df0c831c4f6dbb37df366e95c81891
joshua1 <joshua1@163.com>
2021-12-27 01:38:09 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/git_Test.java
+++ src/main/java/com/atguigu/git/git_Test.java
@@ -4,5 +4,6 @@ public class git_Test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello java");
+        System.out.println("hello hadoop");
     }
 }
