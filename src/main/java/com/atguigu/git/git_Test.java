6361a88eb03ec84d9c6b4f73aaf089e5ffa45f15
joshua1 <joshua1@163.com>
2021-12-27 01:21:41 +0800

    second commit

--- src/main/java/com/atguigu/git/git_Test.java
+++ src/main/java/com/atguigu/git/git_Test.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class git_Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello java");
     }
 }
